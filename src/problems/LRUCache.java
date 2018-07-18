b55cf22a9b7b2b5b3a1a9aceaa9e119d89cec129
winterszhangdong <shifenjiandan@gmail.com>
2018-07-18 22:10:57 +0900

    problem 146 solved

--- src/problems/LRUCache.java
+++ src/problems/LRUCache.java
@@ -32,19 +32,17 @@ public class LRUCache {
     private int capacity;
     private HashMap<Integer, ListNode> cache;
     private ListNode head;
+    private ListNode tail;
 
-    // from small to big
     class ListNode {
         public int key;
         public int value;
-        public int count;
         public ListNode next;
         public ListNode pre;
 
-        ListNode(int k, int v, int c) {
+        ListNode (int k, int v) {
             this.key = k;
             this.value = v;
-            this.count = c;
             this.next = null;
             this.pre = null;
         }
@@ -54,49 +52,63 @@ public class LRUCache {
         this.capacity = capacity;
         this.cache = new HashMap<Integer, ListNode>();
         this.head = null;
+        this.tail = null;
     }
 
     public int get(int key) {
         if (this.cache.containsKey(key)) {
             ListNode node = this.cache.get(key);
-            ListNode nextNode = node.next;
-            node.count++;
-            if (nextNode != null && node.count > node.next.count) {
-                nextNode.pre = node.pre;
-                node.next = nextNode.next;
-                node.pre = nextNode;
-                nextNode.next = node;
-                if (node == this.head)
-                    this.head = nextNode;
-            }
-            System.out.println("get " + key + ": " + node.value);
+            this.moveToHead(node);
             return node.value;
         } else {
-            System.out.println("get " + key + ": -1");
             return -1;
         }
     }
 
     public void put(int key, int value) {
+        if (this.capacity <= 0)
+            return;
+
+        if (this.cache.isEmpty()) {
+            ListNode node = new ListNode(key, value);
+            this.head = node;
+            this.tail = node;
+            this.cache.put(key, node);
+            return;
+        }
+
         if (this.cache.containsKey(key)) {
-            this.cache.get(key).value = value;
+            ListNode node = this.cache.get(key);
+            node.value = value;
+            this.moveToHead(node);
         } else if (this.cache.size() < this.capacity) {
-            ListNode node = new ListNode(key, value, 1);
+            ListNode node = new ListNode(key, value);
             node.next = this.head;
-            if (this.head != null)
-                this.head.pre = node;
+            this.head.pre = node;
             this.head = node;
             this.cache.put(key, node);
         } else {
-            this.cache.remove(this.head.key);
-            ListNode node = this.head;
-            while (node.next != null && node.next.value == 1)
-                node = node.next;
-//            if (node.next == null)
-            this.head.value = value;
-            this.head.count = 1;
-            this.cache.put(key, this.head);
+            this.cache.remove(this.tail.key);
+            ListNode node = this.tail;
+            node.value = value;
+            node.key = key;
+            moveToHead(node);
+            this.cache.put(key, node);
         }
-        System.out.println("put key: " + key + " value: " + this.cache.get(key).value);
+    }
+
+    private void moveToHead(ListNode node) {
+        if (node == this.head)
+            return;
+
+        node.pre.next = node.next;
+        if (node != this.tail)
+            node.next.pre = node.pre;
+        else
+            this.tail = node.pre;
+
+        node.next = this.head;
+        this.head.pre = node;
+        this.head = node;
     }
 }
