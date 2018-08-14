6ce27ff7e102bb58cf8c6aa420761200b90d143d
winterszhangdong <shifenjiandan@gmail.com>
2018-08-14 22:02:33 +0900

    added solution of problem 67

--- src/problems/AddBinary.java
+++ src/problems/AddBinary.java
@@ -8,11 +8,10 @@ package problems;
  * The input strings are both non-empty and contains only characters 1 or 0.
  *
  * Example 1:
- *
  * Input: a = "11", b = "1"
  * Output: "100"
- * Example 2:
  *
+ * Example 2:
  * Input: a = "1010", b = "1011"
  * Output: "10101"
  */
